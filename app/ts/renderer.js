cd2642fa37455e7ddba04cd3999df07bd8cf9c11
Charlie <75177875+TheTrueGravity@users.noreply.github.com>
2021-09-19 12:55:31 +0100

    main

--- app/ts/renderer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const custom_electron_titlebar_1 = require("custom-electron-titlebar");
-new custom_electron_titlebar_1.Titlebar({
-    backgroundColor: custom_electron_titlebar_1.Color.fromHex('#444')
-});
--- app/ts/renderer.ts
+++
@@ -1,5 +0,0 @@
-import { Titlebar, Color } from 'custom-electron-titlebar'
-
-new Titlebar({
-    backgroundColor: Color.fromHex('#444')
-})
